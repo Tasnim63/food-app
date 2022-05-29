02e4cf1c18683800c0915b582e77a153f4ffe9af
tasnim63 <tamanna202163@gmail.com>
2022-05-29 17:35:20 +0600

    footer comited

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
              <div className=' bg-dark p-5'>
                  <h4 className=' text-center text-light'>Contact Us</h4>
                 <div>
-                <p className=' text-white font-serif'>Copyright © 2022 - All right reserved </p>
+                <p className=' text-center text-white font-serif'>Copyright © 2022 - All right reserved </p>
                  </div>
               </div>
        </>
