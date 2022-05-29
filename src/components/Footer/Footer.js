870d7bfcebdf9b36a916cf1bb2277173745db069
tasnim63 <tamanna202163@gmail.com>
2022-05-29 17:30:39 +0600

    hh

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,13 @@
 import React from 'react';
-
+import { FiFacebook } from 'react-icons/fi';
 const Footer = () => {
     return (
         <>
              <div className=' bg-dark p-5'>
                  <h4 className=' text-center text-light'>Contact Us</h4>
-            
+                <div>
+                <p className=' text-white font-serif'>Copyright © 2022 - All right reserved </p>
+                 </div>
               </div>
        </>
     );
