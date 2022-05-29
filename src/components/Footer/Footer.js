c401648602d4f7086fcc1f99f47340f7eadcb8a1
tasnim63 <tamanna202163@gmail.com>
2022-05-29 17:21:07 +0600

    custom link

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <>
+             <div className=' bg-dark p-5'>
+                 <h4 className=' text-center text-light'>Contact Us</h4>
+            
+              </div>
+       </>
+    );
+};
+
+export default Footer;
